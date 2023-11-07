6ed90e8fc20aad4e76951f694ebfd2bf74979507
sumantestbook <suman@testbook.com>
2023-11-07 18:09:57 +0530

    added form

--- src/app/router/login/login.component.ts
+++ src/app/router/login/login.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm:FormGroup
+  regexName:string = "^[A-Za-z\\s]{2,25}$";
+  regexPass:string = "^(?=.*[A-Z!@#$%^&*])[A-Za-z\\d!@#$%^&*]{6,25}$";
+  constructor(private fb: FormBuilder) {
+    this.loginForm =  this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(this.regexName)]],
+      password: ['', [Validators.required, Validators.pattern(this.regexPass)]]
+    })
+  }
 
+  ngOnInit() {
+
+  }
+
+
+  onSubmit(form:FormGroup){
+    console.log(form.invalid);
+  }
 }
---
+++ src/app/router/login/login.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {LoginComponent} from "./login.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {LoginRoutingModule} from "./login-routing.module";
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    LoginRoutingModule
+  ]
+})
+export class LoginModule { }
